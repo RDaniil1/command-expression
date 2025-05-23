7a3ad4170c18b6a65efb4d517dec32875e3ced16
RDaniil1 <>
2025-05-23 12:44:57 +0300

    main: add voice functionality

--- calculate.py
+++ calculate.py
@@ -1,5 +1,3 @@
-#latex is also needed
- 
 import sys
 
 from PIL import Image, ImageDraw, ImageFont
@@ -7,14 +5,19 @@ from PIL import Image, ImageDraw, ImageFont
 from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt6.uic.load_ui import loadUi
 from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
 
+from contextlib import redirect_stdout
 from pathlib import Path 
 
 from sympy import preview
 from ru_word2number import w2n
 
+import speech_recognition as sr
+import pyaudio
 
-symbol_dispatcher = {
+
+command_dispatcher = {
     'разделить' : '/',
     'умножить' : '*', 
     'плюс' : '+',
@@ -24,42 +27,85 @@ symbol_dispatcher = {
     'конец' : ')'
 }
 
+symbol_dispatcher = {
+     '/' : 'разделить',
+     '*' : 'умножить', 
+     '+' : 'плюс',
+     '-' : 'минус',
+     '**' : 'степень',
+     '(' : 'начало',
+     ')' : 'конец',
+     '_' : ' '
+}
+
 class MainWindow(QMainWindow):
-    
     def __init__(self):
         super(QMainWindow, self).__init__()
         
         self.setWindowTitle('Расчёт выражения')
         loadUi(self.UI_PATH, self)
         
-        self.okButton.clicked.connect(self.get_expression_and_result)
+        self.okButton.clicked.connect(lambda: self.get_expression_and_result('text'))
+        self.voiceButton.clicked.connect(lambda: self.get_expression_and_result('voice'))        
 
         self.calculation_path = str(Path(__file__).parent / "calc.png")
         self.expression_path = str(Path(__file__).parent / "expression.png")
         
     def show_error_window(self, message: str):
         msg_box = QMessageBox()
-        msg_box.setWindowTitle('Error')
+        msg_box.setWindowTitle('Ошибка')
         msg_box.setText(message)
         msg_box.exec()
 
     def create_calculation_image(self, words: list[str]):
         try:
-            preview(eval(''.join(words)), viewer='file', filename=self.calculation_path, dvioptions=["-D 650"])
+            preview(eval(''.join(words)), viewer='file', \
+                    filename=self.calculation_path, dvioptions=["-D 650"])
             self.set_expression(' '.join(words))
 
             img = QPixmap(self.calculation_path)
             self.resultImage.setPixmap(img)
         except:
-            self.show_error_window('Ошибка ввода данных')
+            self.show_error_window('Ошибка ввода данных')  
+    
+    def get_words_from_text(self, mode: str):
+        if mode == 'text':
+            return self.equationEdit.text().split() 
+        elif mode == 'voice':
+            return self.get_text_from_voice().split()
+        else:
+            return ['']
 
-    def get_expression_and_result(self):
-        words = self.equationEdit.text().split()
-        words = [word.replace('_', ' ') for word in words]
+    def get_text_from_voice(self):
+        recognizer = sr.Recognizer()
+
+        with redirect_stdout(None):        
+            text= ''
+            with sr.Microphone() as source:
+                recognizer.adjust_for_ambient_noise(source)
+                audio_data = recognizer.record(source, duration=2)
+
+                text = recognizer.recognize_google(audio_data, language='ru-RU')
+
+        return text
         
-        result = self.check_numbers(words)
-        if result != 'Correct':
-            self.show_error_window(result)
+    def prepare_words_for_calculation(self, words: list[str]):
+        words = [word.replace('_', ' ') for word in words]
+        prepared_words = []
+        for word in words:
+            if word in symbol_dispatcher.keys():
+                word = word.replace(word, symbol_dispatcher[word])
+            prepared_words += [word]
+
+        return prepared_words
+
+    def get_expression_and_result(self, mode: str):
+        words = self.get_words_from_text(mode)
+        words = self.prepare_words_for_calculation(words)
+        
+        failure_status = self.check_numbers(words)
+        if failure_status:
+            self.show_error_window(failure_status)
             return 
 
         words = self.get_replaced_words(words)
@@ -77,8 +123,7 @@ class MainWindow(QMainWindow):
     def create_expression_image(self, expression: str):
         img = Image.new('RGB', (800, 100), color='white')
         draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("arial.ttf", size=35)
-        draw.text((25, 25), expression, (0, 0, 0), font=font)
+        draw.text((25, 25), expression, (0, 0, 0))
         img.save(self.expression_path)
 
     def get_replaced_words(self, equation: list[str]) -> list[str]:
@@ -92,16 +137,16 @@ class MainWindow(QMainWindow):
                 pass
     
         for i in range(len(equation)):
-            value_exists_in_dict = symbol_dispatcher.get(equation[i]) != None
+            value_exists_in_dict = command_dispatcher.get(equation[i]) != None
             if value_exists_in_dict:
-                equation[i] = symbol_dispatcher.get(equation[i])
+                equation[i] = command_dispatcher.get(equation[i])
         
         return equation
 
-    def get_nums_indexes(self, equation: list[str]) -> list[str]:
+    def get_nums_indexes(self, equation: list[str]) -> list[int]:
         num_indexes = []
         for i in range(len(equation)):
-            not_exists_in_dict = symbol_dispatcher.get(equation[i]) == None
+            not_exists_in_dict = command_dispatcher.get(equation[i]) == None
             if not_exists_in_dict:
                 num_indexes += [i]
         
@@ -109,7 +154,7 @@ class MainWindow(QMainWindow):
     
     def check_numbers(self, equation: list[str]):
         num_indexes = self.get_nums_indexes(equation)        
-        equation = [equation[i] for i in range(len(equation)) if i in num_indexes]
+        equation = [equation[i] for i in num_indexes]
             
         equation = sum([number.split() for number in equation], [])
         
@@ -119,7 +164,7 @@ class MainWindow(QMainWindow):
         except ValueError:
             return f"Ошибка при вводе числа. Разряда '{rank}' не сущетсвует"
         
-        return 'Correct'
+        return ''
         
     UI_PATH = Path(__file__).parent / "window.ui"
         
